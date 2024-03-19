732bca639f4c2f6c6f207f80dc25135911e4505d
Gaurav Agarwal <algogrit@gmail.com>
2024-03-19 14:16:51 +0530

    Removing the sol.

--- exercises/exercism/guessing-game/lib/guessing_game.ex
+++ exercises/exercism/guessing-game/lib/guessing_game.ex
@@ -1,8 +1,3 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess \\ :no_guess)
-  def compare(_secret_number, guess) when guess == :no_guess, do: "Make a guess"
-  def compare(secret_number, guess) when secret_number == guess, do: "Correct"
-  def compare(secret_number, guess) when secret_number+1 == guess or secret_number-1 == guess, do: "So close"
-  def compare(secret_number, guess) when secret_number > guess, do: "Too low"
-  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+  def compare(secret_number, guess)
 end
