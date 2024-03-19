d6ee184d0c4982348ce4adfd0479017c1300774d
Gaurav Agarwal <algogrit@gmail.com>
2024-03-19 15:31:17 +0530

    Removing the solution.

--- exercises/exercism/secrets/lib/secrets.ex
+++ exercises/exercism/secrets/lib/secrets.ex
@@ -1,36 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
     # Please implement the secret_add/1 function
-    fn (n) -> n + secret end
   end
 
   def secret_subtract(secret) do
     # Please implement the secret_subtract/1 function
-    fn (n) -> n - secret end
   end
 
   def secret_multiply(secret) do
     # Please implement the secret_multiply/1 function
-    fn (n) -> n * secret end
   end
 
   def secret_divide(secret) do
     # Please implement the secret_divide/1 function
-    fn (n) -> Integer.floor_div n, secret end
   end
 
   def secret_and(secret) do
     # Please implement the secret_and/1 function
-    fn (n) -> Bitwise.band(n, secret) end
   end
 
   def secret_xor(secret) do
     # Please implement the secret_xor/1 function
-    fn (n) -> Bitwise.bxor(n, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
     # Please implement the secret_combine/2 function
-    fn (n) -> n |> secret_function1.() |> secret_function2.() end
   end
 end
