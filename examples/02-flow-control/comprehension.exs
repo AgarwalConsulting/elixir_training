c1ef0adcceaca798749899b014c5e8c3090778e1
Gaurav Agarwal <algogrit@gmail.com>
2024-03-19 10:16:05 +0530

    Updating with filter option.

--- examples/02-flow-control/comprehension.exs
+++ examples/02-flow-control/comprehension.exs
@@ -5,3 +5,13 @@ output = for i <- primes, j <- [:a, :b] do
         end
 
 IO.inspect output
+
+# # ---
+
+# primes = [1, 2, 3, 5, 7]
+
+# output = for i <- primes, j <- primes, i != j do
+#           {j, i}
+#         end
+
+# IO.inspect output
